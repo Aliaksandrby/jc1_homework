506af3328208c90ce452dc3ad754b21942763fbc
Aliaksandrby <aliaksandr.bymail@gmail.com>
2022-04-02 22:42:46 +0300

    fix exercise 46

--- src/by/exercise46/Main.java
+++ src/by/exercise46/Main.java
@@ -1,28 +1,20 @@
 /*
 * Вывести список файлов и каталогов выбранного каталога на диске.
 * Файлы и каталоги должны быть распечатаны отдельно.
-* c:\Users\Hello\Desktop\workspace\java\education\HomeWork\
 *
-* not done yet, need thinking
+* c:\Users\Hello\Desktop\workspace\java\education\HomeWork\
+* c:/Users/Hello/Desktop/workspace/java/education/HomeWork
 */
 
 package by.exercise46;
 
-import java.io.File;
-
 public class Main {
     public static void main(String[] args) {
-
-
-        //String pathToFolder = "c:/Users/Hello/Desktop/workspace/java/education/HomeWork";
-        //UserOut.showFilesAndFolders(pathToFolder);
-
-        File pathToFolder = new File("c:/Users/Hello/Desktop/workspace/java/education/HomeWork");
-        File[] listFilesFolders = pathToFolder.listFiles();
-
-        for (File f : listFilesFolders) {
-            System.out.println(f.isFile() + " : " + f.getName());
-        }
-
+        Console.invitation();
+        String path = Console.inputPath();
+        FoldersFiles foldersFiles = new FoldersFiles(path);
+        foldersFiles.separate();
+        Console.show(foldersFiles.getFiles());
+        Console.show(foldersFiles.getFolders());
     }
 }
