d5e45de8818e9f557a30f45c9efd3fbdcd0f1558
Aliaksandrby <aliaksandr.bymail@gmail.com>
2022-04-06 18:30:07 +0300

    add exercise 49

--- src/by/exercise49/Main.java
+++ src/by/exercise49/Main.java
@@ -1,43 +1,21 @@
 /*
-* Записать с помощью Java в двоичный файл 20 случайных чисел.
-* Прочитать записанный файл, вывести на экран числа и их среднее арифметическое.
-*/
+ * Записать с помощью Java в двоичный файл 20 случайных чисел.
+ * Прочитать записанный файл, вывести на экран числа и их среднее арифметическое.
+ */
 
 package by.exercise49;
 
-import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Numbers numbers = new Numbers();
-
-        for (int count : numbers.getRandomNumbers()) {
-            System.out.print(count + " ");
-        }
-        System.out.println();
-
-        DataOutputStream out = new DataOutputStream(new FileOutputStream("binaryFile"));
-        for (int number : numbers.getRandomNumbers()) {
-            out.write(number);
-        }
-
-        DataInputStream in = new DataInputStream(new FileInputStream("binaryFile"));
-        int[] newArray = new int[20];
-        for (int i = 0; i < newArray.length; i++) {
-            newArray[i] = in.read();
-        }
-
-        for (int num : newArray) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-
-        int sum = 0;
-        for (int num : newArray) {
-            sum += num;
-        }
-        double value = (double)sum/newArray.length;
-        System.out.println(value);
-
+    public static void main(String[] args) {
+        Numbers numbers = new Numbers(20);
+        //Console.show(numbers); // not necessary;
+        OutInBinaryFile outInBinaryFile = new OutInBinaryFile("binaryFile");
+        outInBinaryFile.writeFile(numbers);
+        ArrayList<Integer> numbersFromFile = outInBinaryFile.readFile();
+        Console.showNumbersFromFile(numbersFromFile);
+        double averageArithmetic = numbers.averageArithmetic(numbersFromFile);
+        Console.showAverageArithmetic(averageArithmetic);
     }
 }
