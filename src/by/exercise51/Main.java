4aa0918b74a726adb8ab735e98faeec806acd227
Aliaksandrby <aliaksandr.bymail@gmail.com>
2022-04-09 19:59:06 +0300

    add exercise 51

--- src/by/exercise51/Main.java
+++ src/by/exercise51/Main.java
@@ -1,13 +1,14 @@
 /*
-* Создать объект Person с полями name, surname(), age. Сгенерировать 10 объектов класса
-* Person со случайными полями соответствующего типа. С помощью Java создать файл, в
-* который запишется информация об этих людях.
-*/
+ * Создать объект Person с полями name, surname, age. Сгенерировать 10 объектов класса
+ * Person со случайными полями соответствующего типа. С помощью Java создать файл, в
+ * который запишется информация об этих людях.
+ */
 
 package by.exercise51;
 
 public class Main {
     public static void main(String[] args) {
-
+        GeneratorPerson generatorPerson = new GeneratorPerson(10);
+        generatorPerson.writeFile("personsInfo.txt");
     }
 }
