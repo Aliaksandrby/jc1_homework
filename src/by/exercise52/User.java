a9d26db2b809581f3b401265fb901a233c4fded5
Aliaksandrby <aliaksandr.bymail@gmail.com>
2022-04-10 14:14:05 +0300

    add exercise 52

---
+++ src/by/exercise52/User.java
@@ -0,0 +1,7 @@
+package by.exercise52;
+
+public class User {
+    public static void show(String threadName,double averageArithmetic) {
+        System.out.println(threadName + " = " + averageArithmetic);
+    }
+}
