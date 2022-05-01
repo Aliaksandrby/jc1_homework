40c6825e2ca45c0a12a1a9f025dd83accb777092
Aliaksandrby <aliaksandr.bymail@gmail.com>
2022-05-01 16:29:15 +0300

    fix 17

--- src/by/exercise17/Main.java
+++ src/by/exercise17/Main.java
@@ -8,8 +8,37 @@ package by.exercise17;
 
 public class Main {
     public static void main(String[] args) {
-        int number = 20023143;
-        String str = String.format("%,d",number);
-        System.out.println(str);
+        //System.out.println(String.format("%,d",2002314));
+        System.out.println(Accountant.format(-51233255));
+    }
+}
+
+class Accountant {
+    public static <T extends Number> String format(T number) {
+        String str = number.toString();
+        String newStr = "";
+        int i = str.length();
+        int count = 0;
+        do {
+            count++;
+            if(count == 4) {
+                newStr += ' ';
+                count = 1;
+            }
+            i--;
+            newStr += str.charAt(i);
+        } while (i != 0);
+        newStr = revers(newStr);
+        return newStr;
+    }
+
+    private static String revers(String string) {
+        String str = "";
+        int i = string.length();
+        do {
+            i--;
+            str += string.charAt(i);
+        } while (i != 0);
+        return str;
     }
 }
